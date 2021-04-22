34db59be879024f75e9027c1ce25459588745a11
mjeel <yasminsheik05@gmail.com>
2021-04-22 17:15:05 +0530

    function 1

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -2,7 +2,7 @@
 
 SOURCE components/common.sh
 
-INFO "[\e[1;33msetup frontend component\e[0m]"
-INFO  "[\e[1;33mInstalling Nginx\e[0m]"
-SUCC "[\e[1;32mInstalled Nginx\e[0m]"
-FAIL "[\e[1;31mInstalled Nginx\e[0m]"
+INFO "frontend component"
+INFO "Installing Nginx"
+SUCC "Installed Nginx"
+FAIL "Installed Nginx"

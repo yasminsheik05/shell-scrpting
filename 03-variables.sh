730f97dedf7b62472a2e63e618c324e9f49eec98
mjeel <yasminsheik05@gmail.com>
2021-04-22 13:59:37 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,3 +3,15 @@ COURSE_NAME=Devops
 echo welcome to $COURSE_NAME training
 echo $COURSE_NAME training timings-6Am
 echo $COURSE_NAME Trainer -Raghu
+
+
+DATE=$(date +%F)
+echo "good afternoon ,today date is $DATE"
+ADD=$((2+3))
+echo ADD = $ADD
+
+a=10
+b=xyz
+c=true
+d=9.99
+echo a=$a , b=$b , c=$c , d=$d

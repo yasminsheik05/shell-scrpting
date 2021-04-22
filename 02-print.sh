55d33b3e6cc7017c46041f92324261bb79c61759
mjeel <yasminsheik05@gmail.com>
2021-04-22 13:35:25 +0530

    print 2

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,11 @@ echo Hello world
 echo gud afternoon!
 echo -e "Hello, \n\ngud aftrenoon to session \n Regards, \nYasmin"
 
+
+
+echo -e "\e[31mRed text"
+echo -e "\e[32mYellow text"
+echo -e "\e[33mGreen text"
+echo -e "\e[34mBlue text"
+echo -e "\e[35mMagenta text"
+echo -e "\e[36mCyan"

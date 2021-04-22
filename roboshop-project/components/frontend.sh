696b661c79bb5a77292e9e89d015234c8b441497
mjeel <yasminsheik05@gmail.com>
2021-04-22 16:37:18 +0530

    frontend 1.1

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 echo -e [INFO] [Frontend] [$(date '+%F %T')] Installing frontend component
-echo -e "[\e[1;33mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32mInstalling frontend component\e[0m"
+echo -e "[\e[1;33mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32msetup frontend component\e[0m]"
+echo -e "[\e[1;32mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32mInstalling Nginx\e[0m]"
+echo -e "[\e[1;31mSUCC \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32mInstalled Nginx\e[0m]"
+echo -e "[\e[1;34mFAIL \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32msetup frontend component\e[0m]"

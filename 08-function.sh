5d813038befefd5d58f0463bf575fcd865e51776
mjeel <yasminsheik05@gmail.com>
2021-04-22 16:45:54 +0530

    function

---
+++ 08-function.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function SAMPLE() {
+     echo B=$b
+     echo Hello from SAMPLE function
+     a=10
+}
+SAMPLE1() {
+     echo Hello from SAMPLE1 function
+
+}
+
+b=20
+SAMPLE
+SAMPLE2
+echo A=$a

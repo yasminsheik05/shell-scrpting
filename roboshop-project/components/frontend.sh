82787be7e23dbe605be4bb634573eb6b7c550f8d
mjeel <yasminsheik05@gmail.com>
2021-04-22 16:27:13 +0530

    frontend 1

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo [INFO] [Frontend] [$(date "+%F %T")] Installing frontend component
+echo -e [INFO] [Frontend] [$(date '+%F %T')] Installing frontend component
+echo -e "[\e[1;33mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m]"

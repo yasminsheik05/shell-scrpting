1b06e4dad7d4a94201c16e641b1d745156144ad4
mjeel <yasminsheik05@gmail.com>
2021-04-22 17:07:49 +0530

    function 1

--- roboshop-project/components/frontend.sh
+++ roboshop-project/components/frontend.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
-echo -e [INFO] [Frontend] [$(date '+%F %T')] Installing frontend component
-echo -e "[\e[1;33mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32msetup frontend component\e[0m]"
-echo -e "[\e[1;32mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32mInstalling Nginx\e[0m]"
-echo -e "[\e[1;31mSUCC \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32mInstalled Nginx\e[0m]"
-echo -e "[\e[1;34mFAIL \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] [\e[1;32mInstalled Nginx\e[0m]"
+INFO() {
+  echo -e "[\e[1;33mINFO \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+SUCC() {
+  echo -e "[\e[1;32mSUCC \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+FAIL() {
+  echo -e "[\e[1;31mFAIL \e[0m] [\e[1;35mFrontend\e[0m] [\e[1;36m$(date '+%F %T')\e[0m] $1"
+}
+INFO "[\e[1;33msetup frontend component\e[0m]"
+INFO  "[\e[1;33mInstalling Nginx\e[0m]"
+SUCC "[\e[1;32mInstalled Nginx\e[0m]"
+FAIL "[\e[1;31mInstalled Nginx\e[0m]"

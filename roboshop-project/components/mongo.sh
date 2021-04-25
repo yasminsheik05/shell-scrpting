b03cf756dfc5196fdbac58b8273c2c6e42fc1a9c
mjeel <yasminsheik05@gmail.com>
2021-04-25 14:22:06 +0530

    frontend

--- roboshop-project/components/mongo.sh
+++ roboshop-project/components/mongo.sh
@@ -2,17 +2,3 @@
 
 COMPONENT=Mongo
 source components/common.sh
-
-INFO "Setup Mongo component"
-INFO "Setup mongoDB yum repository"
-echo '[mongodb-org-4.2]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' >/etc/yum.repos.d/mongodb.repo
-
-STAT $? "Repository setup"
-INFO "Installing mongoDB"
-yum install -y mongodb.org &>>$LOG_FILE
-STAT $? "MongoDB Install"

ea8ebd964af26b6ba7aad88fce8de3fbe8c2dfa1
mjeel <yasminsheik05@gmail.com>
2021-04-26 10:01:48 +0530

    Mongo

--- roboshop-project/components/mongo.sh
+++ roboshop-project/components/mongo.sh
@@ -30,7 +30,15 @@ STAT $? "MongoDB Restart"
 INFO "Downloading MongoDB Schema"
 DOWNLOAD_ARTIFACT  "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/03f2af34-e227-44b8-a9f2-c26720b34942/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
 
+cd /tmp
+INFO "Extract Artifact"
+unzip -o mongodb.zip &>>$LOG_FILE
+STAT $? "Artifact Extract"
 
-# systemctl enable mongod
-# systemctl start mongod
-#/etc/mongod.conf from 127.0.0.1 to 0.0.0.0
+INFO "Load shema-catalogue service"
+mongo < catalogue.js &>>$LOG_FILE
+STAT $? "Catalogue Schema Load"
+
+INFO "Load Schema - User Service"
+mongo < users.js &>>$LOG_FILE
+STAT $? "User Schema Load"

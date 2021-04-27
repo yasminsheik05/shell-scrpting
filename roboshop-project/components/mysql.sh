12f011e4d3e486a25a5442f025e501ca30ef6773
mjeel <yasminsheik05@gmail.com>
2021-04-27 13:35:58 +0530

    mysql

--- roboshop-project/components/mysql.sh
+++ roboshop-project/components/mysql.sh
@@ -39,3 +39,12 @@ case $? in
     STAT $? "Password Reset"
   ;;
 esac
+
+INFO "Download mysql schema"
+DOWNLOAD_ARTIFACT "https://dev.azure.com/DevOps-Batches/f635c088-1047-40e8-8c29-2e3b05a38010/_apis/git/repositories/2235ab8a-3229-47d9-8065-b56713ed7b28/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
+
+INFO "Load schema"
+cd /tmp
+unzip-o mysql.zip &>>$LOG_FILE
+mysql -u root -ppassword <shipping.sql &>>$LOG_FILE
+STAT $? "Schema download"

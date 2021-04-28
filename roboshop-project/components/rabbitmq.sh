f3b750a4b96760103b94f60b0eb456ab46e8b62b
mjeel <yasminsheik05@gmail.com>
2021-04-28 12:18:56 +0530

    rabbitmq

---
+++ roboshop-project/components/rabbitmq.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+COMPONENT=rabbitmq
+source components/common.sh
+
+INFO "Setup RabbitMQ component"
+
+INFO "Install Erlang"
+yum install https://packages.erlang-solutions.com/erlang/rpm/centos/7/x86_64/esl-erlang_22.2.1-1~centos~7_amd64.rpm -y &>>$LOG_FILE
+STAT $? "Erlang Installation"
+
+INFO "Setup RabbitMQ Installation"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>$LOG_FILE
+STAT $? "RabbitMQ Yum Repos Setup"
+
+INFO "Install RabbitMQ Server"
+yum install rabbitmq-server -y &>>$LOG_FILE
+STAT $? "RabbitMQ Server Install"
+
+INFO "Start RabbitMQ Server"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server &>>$LOG_FILE
+STAT $? "Start RabbitMQ Service"
+
+INFO "Create Roboshop App User In RabbitMQ"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+STAT $? "Roboshop App User Create"

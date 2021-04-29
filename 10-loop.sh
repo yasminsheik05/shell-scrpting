2deef752a181a635115131736accda36ca7d4a2f
mjeel <yasminsheik05@gmail.com>
2021-04-29 12:39:15 +0530

    instance

---
+++ 10-loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##for loop
+for i in frontend catalogue mongo
+do
+  echo component = $i
+  sleep1
+done
---
+++ roboshop-project/launch-instance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case $1 in
+  launch)
+    for component in frontend catalogue mongo cart user shipping payment mysql rabbitmq redis; do
+      aws ec2 run-instances --launch-template LaunchTemplateId=lt-025292828ff70c560 --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=frontend}]"
+      done
+    ;;
+    routes)
+      echo updating routes
+    ;;
+  esac

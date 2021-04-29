3405d41205e7e234a8bc0987e7a7361e28e7a9b9
mjeel <yasminsheik05@gmail.com>
2021-04-29 14:57:41 +0530

    instance

--- roboshop-project/launch-instance.sh
+++ roboshop-project/launch-instance.sh
@@ -3,14 +3,13 @@
 case $1 in
   launch)
     for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
-      echo "Launching $component Spot instance"
+      echo "Launching $component Spot Instance"
       aws ec2 run-instances --launch-template LaunchTemplateId=lt-025292828ff70c560 --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${component}}]" &>>/tmp/instance-launch
       done
     ;;
     routes)
       echo updating routes
        for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
-      echo "Launching $component Spot instance"
       IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq  '.Reservations[].Instances[].PrivateIpAddress')
       echo $component $IP
       done

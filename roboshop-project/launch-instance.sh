1f17716bcf84c2738c5e516b7fdfd5e494b56121
mjeel <yasminsheik05@gmail.com>
2021-04-29 14:53:49 +0530

    instance

--- roboshop-project/launch-instance.sh
+++ roboshop-project/launch-instance.sh
@@ -9,5 +9,11 @@ case $1 in
     ;;
     routes)
       echo updating routes
+       for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
+      echo "Launching $component Spot instance"
+      IP=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${component} Name=instance-state-name,Values=running | jq  '.Reservations[].Instances[].PrivateIpAddress')
+      echo $component $IP
+      done
     ;;
   esac
+done

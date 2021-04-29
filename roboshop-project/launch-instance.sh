ff494af58fbb6044cb6c66f14f31eca1eb51ea41
mjeel <yasminsheik05@gmail.com>
2021-04-29 14:16:44 +0530

    instance

--- roboshop-project/launch-instance.sh
+++ roboshop-project/launch-instance.sh
@@ -3,7 +3,7 @@
 case $1 in
   launch)
     for component in frontend catalogue cart user shipping payment mysql mongo rabbitmq redis; do
-      echo "Launching $component sopt instance"
+      echo "Launching $component Spot instance"
       aws ec2 run-instances --launch-template LaunchTemplateId=lt-025292828ff70c560 --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${component}}]" &>>/tmp/instance-launch
       done
     ;;
@@ -11,4 +11,3 @@ case $1 in
       echo updating routes
     ;;
   esac
-done
